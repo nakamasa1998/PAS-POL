760cf0fb935308610b0c6cd025ba66a143a89566
nakamasa1998 <87458179+nakamasa1998@users.noreply.github.com>
2021-07-17 10:47:11 +0900

    Initial commit.
    初めてのコミットです。

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+	autoplay: {
+		delay: 4000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+		reverseDirection: false,
+	},
+});
